2e1d83f3280ba2227a2cb98c908b6e2931a55a6f
pedrinabrasil <pedrinabrasil@gmail.com>
2019-09-10 10:18:14 -0300

    crud tipo de atividades

--- app/controllers/Administrators.java
+++ app/controllers/Administrators.java
@@ -31,6 +31,82 @@ public class Administrators extends Attendants {
 		renderArgs.put("event", event);
 	}
 
+	public static void listActivityTypes() {
+		flash.clear();
+		List<ActivityType> types = ActivityType.findAll();
+		renderArgs.put("types", types);
+		renderTemplate("Administrators/listActivityTypes.html");
+	}
+	
+	public static void newActivityType() {
+		flash.clear();
+		ActivityType activitytype = new ActivityType();
+		renderArgs.put("activitytype", activitytype);
+        renderTemplate("Administrators/newActivityType.html");
+	}
+	
+	public static void editActivityType(Long id) {
+		flash.clear();
+		ActivityType activitytype = ActivityType.findById(id);
+		renderArgs.put("activitytype", activitytype);
+        renderTemplate("Administrators/editActivityType.html");
+	}
+	
+	public static void removeActivityType(Long id) {
+		Long count = (Long) JPA.em().createQuery("select count (a) from Activity a where a.type.id = "+id).getSingleResult();
+		if(count > 0) {
+			List<ActivityType> types = ActivityType.findAll();
+			renderArgs.put("types", types);
+			flash.error("erro.dependencia");
+			renderTemplate("Administrators/listActivityTypes.html");
+		}
+		ActivityType activitytype = ActivityType.findById(id);
+		activitytype.delete();
+		listActivityTypes();
+	}
+	
+	
+	public static void saveActType(@Valid ActivityType activitytype) {
+		boolean isNew = activitytype.id == null || activitytype.id  == 0;
+		if (validation.hasErrors() ) {
+			System.out.println(validation.errorsMap());
+            flash.error("erro.operacao");
+            
+            renderArgs.put("activitytype", activitytype);
+            if(isNew) {
+            	renderTemplate("Administrators/newActivityType.html");
+            }else {
+            	renderTemplate("Administrators/editActivityType.html");	
+            }
+            
+            
+        }
+		activitytype.save();
+		flash.success("applicaiton.success");
+		listActivityTypes();
+	}
+	
+	public static void saveActivityType(String name, String color) {
+		ActivityType at = new ActivityType();
+		at.name = name;
+		at.color = color;
+		at.save();
+		flash.success("event.success");
+        renderJSON(at);
+	}
+	
+	public static void listEvents() {
+		List<Event> events = Event.all().fetch();
+		renderArgs.put("events", events);
+		renderTemplate("Administrators/listEvents.html");
+	}
+	
+	public static void editEvent(Long id) {
+		Event e = Event.findById(id);
+		renderArgs.put("e", e);
+		render();
+	}
+	
 	public static void saveEvent(@Valid Event e) {
 		validation.future(e.end, e.start);
 		if (validation.hasErrors() ) {
@@ -43,9 +119,45 @@ public class Administrators extends Attendants {
 		flash.success("event.success");
         listEvents();
 	}
+	
+	public static void listActivities(Long eventid) {
+		List<Activity> activities = Activity.find("select a from Activity a where a.event.id = "+eventid).fetch();
+		renderArgs.put("activities", activities);
+		renderTemplate("Administrators/listActivities.html");
+	}
 
+	public static void editActivity(Long id) {
+		Activity a = Activity.findById(id);
+		renderArgs.put("activity", a);
+		
+		List<ActivityType> activityTypes = ActivityType.findAll();
+		renderArgs.put("types", activityTypes);
+		
+		List<ActivityStatus> status = ActivityStatus.list();
+		renderArgs.put("status", status);
+		
+		render();
+	}
+	
+	public static void newActivity() {
+		Activity a = new Activity();
+		a.status = ActivityStatus.DRAFT;
+		Event event = Event.findById(Long.parseLong(session.get("eventid")));
+		a.event =event; 
+		a.startInscription = new Date();
+		
+		renderArgs.put("activity", a);
+		
+		List<ActivityType> activityTypes = ActivityType.findAll();
+		renderArgs.put("types", activityTypes);
+		
+		List<ActivityStatus> status = ActivityStatus.list();
+		renderArgs.put("status", status);
+		
+		render();
+	}
 	public static void saveActivity(@Valid Activity activity, String facilitators) {
-		boolean isNewActivity = activity.uuid == null || activity.uuid == 0;
+		boolean isNew = activity.uuid == null || activity.uuid == 0;
 		if (validation.hasErrors() ) {
 			System.out.println(validation.errorsMap());
             flash.error("application.error");
@@ -57,7 +169,7 @@ public class Administrators extends Attendants {
     		List<ActivityStatus> status = ActivityStatus.list();
     		renderArgs.put("status", status);
     		
-            if(isNewActivity) {
+            if(isNew) {
 				renderTemplate("Attendees/editActivity.html");
 			}else {
 				renderTemplate("Administrators/newActivity.html");
@@ -78,107 +190,14 @@ public class Administrators extends Attendants {
 		listActivities(activity.event.id);
 	}
 	
-	public static void saveTrack(@Valid Track track, Criteria[] oCriterias, Criteria[] nCriterias) {
-
-		validation.future(track.end, track.start);
-		if (validation.hasErrors() ) {
-			System.out.println(validation.errorsMap());
-            flash.error("erro.operacao");
-            renderArgs.put("track", track);
-            List<TrackType> trackTypes = TrackType.list();
-    		renderArgs.put("tktypes", trackTypes);
-    		
-    		List<CalculationType> calcTypes = CalculationType.list();
-    		renderArgs.put("calcTypes", calcTypes);
-            
-    		List<TrackStatus> trackStatus = TrackStatus.list();
-    		renderArgs.put("tkStatus", trackStatus);
-            
-    		renderTemplate("Administrators/editTrack.html");
-        }
-        
-		List<Criteria> criterias = Criteria.find("select c from Criteria c where c.track.id = "+track.id).fetch();
-		
-		//atual 
-		for (Criteria c : criterias) { 
-    	    c.track = null;
-    	    c.save();
-    	} 
-		
-		track.save();
-		
-		//antigas
-		if(oCriterias != null) {
-			for (Criteria c : oCriterias) {
-				Criteria update = Criteria.findById(c.getId());
-				update.track = track;
-				update.save();
-			} 
-		}
-		
-		
-		//novas
-		if(nCriterias != null) {
-			for (Criteria c : nCriterias) {
-				Criteria nova = new Criteria(c.description, c.weight);
-				nova.track = track;
-				nova.save();
-			} 
-		}
-		
-		JPA.em().createQuery("delete from Criteria c where c.track is null").executeUpdate();
 	
-		flash.success("application.success");
-		listTracks(track.event.id);
+	public static void listTracks(Long eventid) {
+		List<Track> tracks = Track.find("select t from Track t where t.event.id = "+eventid).fetch();
+		renderArgs.put("tracks", tracks);
+		renderTemplate("Administrators/listTracks.html");
 	}
+
 	
-	public static void saveUser(@Valid User user) {
-		if (validation.hasErrors() ) {
-			System.out.println(validation.errorsMap());
-            flash.error("erro.operacao");
-            renderArgs.put("u", user);
-            renderTemplate("Administrators/editUser.html");
-        }
-		user.save();
-		flash.success("event.success");
-        listUsers();
-	}
-
-	public static void editEvent(Long id) {
-		Event e = Event.findById(id);
-		renderArgs.put("e", e);
-		render();
-	}
-
-	public static void editActivity(Long id) {
-		Activity a = Activity.findById(id);
-		renderArgs.put("activity", a);
-		
-		List<ActivityType> activityTypes = ActivityType.findAll();
-		renderArgs.put("types", activityTypes);
-		
-		List<ActivityStatus> status = ActivityStatus.list();
-		renderArgs.put("status", status);
-		
-		render();
-	}
-	public static void newActivity() {
-		Activity a = new Activity();
-		a.status = ActivityStatus.DRAFT;
-		Event event = Event.findById(Long.parseLong(session.get("eventid")));
-		a.event =event; 
-		a.startInscription = new Date();
-		
-		renderArgs.put("activity", a);
-		
-		List<ActivityType> activityTypes = ActivityType.findAll();
-		renderArgs.put("types", activityTypes);
-		
-		List<ActivityStatus> status = ActivityStatus.list();
-		renderArgs.put("status", status);
-		
-		render();
-	}
 	
 	public static void editTrack(Long id) {
 		List<TrackType> trackTypes = TrackType.list();
@@ -223,6 +242,63 @@ public class Administrators extends Attendants {
 		renderArgs.put("track", t);
 		render();
 	}
+
+	public static void saveTrack(@Valid Track track, Criteria[] oCriterias, Criteria[] nCriterias) {
+		boolean isNew = track.id == null || track.id == 0;
+		validation.future(track.end, track.start);
+		if (validation.hasErrors() ) {
+			System.out.println(validation.errorsMap());
+            flash.error("erro.operacao");
+            renderArgs.put("track", track);
+            List<TrackType> trackTypes = TrackType.list();
+    		renderArgs.put("tktypes", trackTypes);
+    		
+    		List<CalculationType> calcTypes = CalculationType.list();
+    		renderArgs.put("calcTypes", calcTypes);
+            
+    		List<TrackStatus> trackStatus = TrackStatus.list();
+    		renderArgs.put("tkStatus", trackStatus);
+            if(isNew) {
+            	renderTemplate("Administrators/newTrack.html");
+            }else {
+            	renderTemplate("Administrators/editTrack.html");
+            }
+        }
+        
+		List<Criteria> criterias = Criteria.find("select c from Criteria c where c.track.id = "+track.id).fetch();
+		
+		//atual 
+		for (Criteria c : criterias) { 
+    	    c.track = null;
+    	    c.save();
+    	} 
+		
+		track.save();
+		
+		//antigas
+		if(oCriterias != null) {
+			for (Criteria c : oCriterias) {
+				Criteria update = Criteria.findById(c.getId());
+				update.track = track;
+				update.save();
+			} 
+		}
+		
+		
+		//novas
+		if(nCriterias != null) {
+			for (Criteria c : nCriterias) {
+				Criteria nova = new Criteria(c.description, c.weight);
+				nova.track = track;
+				nova.save();
+			} 
+		}
+		
+		JPA.em().createQuery("delete from Criteria c where c.track is null").executeUpdate();
+	
+		flash.success("application.success");
+		listTracks(track.event.id);
+	}
 	
 	public static void editUser(Long id) {
 		
@@ -234,37 +310,27 @@ public class Administrators extends Attendants {
 		render();
 	}
 
-	public static void listEvents() {
-		List<Event> events = Event.all().fetch();
-		renderArgs.put("events", events);
-		renderTemplate("Administrators/listEvents.html");
-	}
+	
 	
 	public static void listUsers() {
 		List<User> users = User.all().fetch();
 		renderArgs.put("users", users);
 		renderTemplate("Administrators/listUsers.html");
 	}
-	
-	
-	public static void listActivities(Long eventid) {
-		List<Activity> activities = Activity.find("select a from Activity a where a.event.id = "+eventid).fetch();
-		renderArgs.put("activities", activities);
-		renderTemplate("Administrators/listActivities.html");
-	}
-	
-	public static void listTracks(Long eventid) {
-		List<Track> tracks = Track.find("select t from Track t where t.event.id = "+eventid).fetch();
-		renderArgs.put("tracks", tracks);
-		renderTemplate("Administrators/listTracks.html");
-	}
-	
-	public static void listPapers(Long eventid) {
+		
 
-		List<Paper> papers = Paper.find("select p from Paper p where p.track.event.id = "+eventid).fetch();
-		renderArgs.put("papers", papers);
-		renderTemplate("Administrators/listPapers.html");
+	public static void saveUser(@Valid User user) {
+		if (validation.hasErrors() ) {
+			System.out.println(validation.errorsMap());
+            flash.error("erro.operacao");
+            renderArgs.put("u", user);
+            renderTemplate("Administrators/editUser.html");
+        }
+		user.save();
+		flash.success("event.success");
+        listUsers();
 	}
+
 	
 	public static void carregarActiveUsers(String name) {
 		List<User> _users = User.find("select distinct u from User u where lower(u.name) like '%" + name.toLowerCase()
@@ -276,5 +342,13 @@ public class Administrators extends Attendants {
 		}
 		renderJSON(users);
 	}
+	
+	public static void listPapers(Long eventid) {
+
+		List<Paper> papers = Paper.find("select p from Paper p where p.track.event.id = "+eventid).fetch();
+		renderArgs.put("papers", papers);
+		renderTemplate("Administrators/listPapers.html");
+	}
+
 
 }
--- app/models/ActivityType.java
+++ app/models/ActivityType.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 import net.sf.oval.constraint.NotNull;
+import play.data.binding.As;
+import play.data.validation.MaxSize;
 import play.data.validation.Required;
 import play.data.validation.Unique;
 import play.db.jpa.Model;
@@ -17,9 +19,8 @@ public class ActivityType extends Model{
 	@NotNull
 	@Unique
 	@Required(message="validacao.requerido")
+	@MaxSize(50)
 	public String name;
-
-	@NotNull
-	@Required(message="validacao.requerido")
+	
 	public String color;
 }
